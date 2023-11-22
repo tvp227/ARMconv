06cac65e62402e5641a7430a5dc9a250e91cb208
Tom Porter <Tom.Porter@satisnet.co.uk>
2023-11-22 17:16:45 +0000

    Gui Change

--- ConverterGUI.py
+++ ConverterGUI.py
@@ -63,24 +63,27 @@ def count_json_files(directory):
     return json_count, schema_count
 
 # Function to display JSON file and schema count information in the treeview
-def display_results_on_treeview(directory, tree):
+def display_results_on_treeview(directory, tree_json_count, tree_schema_count):
     json_count, schema_count = count_json_files(directory)
 
-    # Clear existing items in the treeview
-    for item in tree.get_children():
-        tree.delete(item)
+    # Clear existing items in the first treeview (JSON file count)
+    for item in tree_json_count.get_children():
+        tree_json_count.delete(item)
 
-    # Insert new items in the treeview for JSON file count
+    # Clear existing items in the second treeview (schema count)
+    for item in tree_schema_count.get_children():
+        tree_schema_count.delete(item)
+
+    # Insert new items in the first treeview for JSON file count
     for category, count in json_count.items():
-        tree.insert("", "end", values=[category, count])
+        tree_json_count.insert("", "end", values=[category, count])
 
-    # Insert new items in the treeview for schema count
+    # Insert new items in the second treeview for schema count
     for schema, count in schema_count.items():
-        tree.insert("", "end", values=[schema, count])
+        tree_schema_count.insert("", "end", values=[schema, count])
 
 class MyGUI:
     def __init__(self, master):
-        # GUI initialization method
         self.master = master
         master.title("Sentinel DevOps Tooling")
         master.geometry("800x600")
@@ -95,35 +98,29 @@ class MyGUI:
         self.background_label = tk.Label(self.background_frame, image=self.background_image)
         self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        # Create a label with the title
         title_label = ttk.Label(master, text="Sentinel DevOps Tooling", font=("Helvetica", 16))
         title_label.place(relx=0.5, rely=0.05, anchor=tk.CENTER)
 
-        # Create a label with the author
         author_label = ttk.Label(master, text="By Tom Porter", font=("Helvetica", 10))
         author_label.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
 
-        # Create a custom style for the buttons
         style = ttk.Style()
         style.theme_use("clam")
 
-        # Configure general button style
         style.configure("TButton",
                         font=("Helvetica", 10),
                         padding=5,
-                        background="#4CAF50",  # Green background
-                        foreground="white"  # White text
+                        background="#4CAF50",
+                        foreground="white"
                         )
 
-        # Configure exit button style separately
         style.configure("Exit.TButton",
                         font=("Helvetica", 10),
                         padding=5,
-                        background="#FF5733",  # Red background
-                        foreground="white"  # White text
+                        background="#FF5733",
+                        foreground="white"
                         )
 
-        # Create buttons using the custom style
         button_spacing = 0.1
         button_y = 0.25
 
@@ -146,113 +143,109 @@ class MyGUI:
         self.convert_DCPB_button = ttk.Button(master, text="Document Creator PB", command=self.launch_DCPB_application, style="TButton")
         self.convert_DCPB_button.place(relx=0.1, rely=button_y, anchor=tk.W)
 
-        # Create an Entry widget to display the selected folder_path
         self.folder_path_var = tk.StringVar()
         self.folder_path_entry = ttk.Entry(master, textvariable=self.folder_path_var, state='readonly', font=("Helvetica", 10))
         self.folder_path_entry.place(relx=0.815, rely=0.65, anchor=tk.CENTER)
 
-        # Create a Browse button to select the folder_path
         self.browse_button = ttk.Button(master, text="Browse", command=self.browse_folder, style="TButton")
         self.browse_button.place(relx=0.78, rely=0.7, anchor=tk.CENTER)
 
-        # Create an exit button
         self.exit_button = ttk.Button(master, text="Exit", command=master.destroy, style="Exit.TButton")
         self.exit_button.place(relx=0.1, rely=0.8, anchor=tk.W)
 
-        # Create a treeview to display JSON file count information
-        self.tree = ttk.Treeview(master, columns=("Category", "Count"), show="headings", height=10)
+        self.tree_json_count = ttk.Treeview(master, columns=("Category", "Count"), show="headings", height=5)
 
-        # Configure column headings
-        self.tree.heading("Category", text="Category", anchor=tk.CENTER)
-        self.tree.heading("Count", text="Count", anchor=tk.CENTER)
+        self.tree_json_count.heading("Category", text="Category", anchor=tk.CENTER)
+        self.tree_json_count.heading("Count", text="Count", anchor=tk.CENTER)
 
-        # Configure treeview style
         style.configure("Treeview",
-                        background="#D3D3D3",  # Light gray background
-                        fieldbackground="#D3D3D3"  # Light gray background for entry widgets
+                        background="#D3D3D3",
+                        fieldbackground="#D3D3D3"
                         )
 
-        # Configure treeview column style
         style.configure("Treeview.Heading",
                         font=("Helvetica", 10, "bold"),
-                        background="#696969",  # Dim gray background for column headings
-                        foreground="white"  # White text for column headings
+                        background="#696969",
+                        foreground="white"
                         )
 
-        # Configure treeview item style
         style.map("Treeview",
-                  background=[("selected", "#347083")],  # Darker color when selected
+                  background=[("selected", "#347083")],
                   foreground=[("selected", "white")]
                   )
 
-        # Place the treeview
-        self.tree.place(relx=0.295, rely=0.42, anchor=tk.W, width=530, height=250)
+        self.tree_json_count.place(relx=0.295, rely=0.42, anchor=tk.W, width=530, height=120)
 
-        # Create a vertical scrollbar
-       ## tree_scrollbar = ttk.Scrollbar(master, orient="vertical", command=self.tree.yview)
-       ## tree_scrollbar.place(relx=0.9, rely=0.37, anchor=tk.E, height=200)                ## Cant get scroll bar working propely 
-        # Configure treeview to use the scrollbar
-       ## self.tree.configure(yscrollcommand=tree_scrollbar.set)
+        self.tree_schema_count = ttk.Treeview(master, columns=("Schema", "Count"), show="headings", height=5)
 
-        # Create a button to refresh the JSON file count information
+        self.tree_schema_count.heading("Schema", text="Schema", anchor=tk.CENTER)
+        self.tree_schema_count.heading("Count", text="Count", anchor=tk.CENTER)
+
+        style.configure("Treeview",
+                        background="#D3D3D3",
+                        fieldbackground="#D3D3D3"
+                        )
+
+        style.configure("Treeview.Heading",
+                        font=("Helvetica", 10, "bold"),
+                        background="#696969",
+                        foreground="white"
+                        )
+
+        style.map("Treeview",
+                  background=[("selected", "#347083")],
+                  foreground=[("selected", "white")]
+                  )
+
+        self.tree_schema_count.place(relx=0.295, rely=0.7, anchor=tk.W, width=530, height=120)
+
         self.refresh_button = ttk.Button(master, text="Refresh", command=self.refresh_json_count, style="TButton")
         self.refresh_button.place(relx=0.92, rely=0.7, anchor=tk.CENTER)
 
-        # Display the initial JSON file count information
         self.refresh_json_count()
 
-    # Launchers
     def launch_playbook_application(self):
-        # Function to launch the Playbook application
         subprocess.run(["python", "Tools/PlayBookConv.py"])
 
     def launch_workbook_application(self):
-        # Function to launch the Workbook application
         subprocess.run(["python", "Tools/WorkBookConv.py"])
 
     def launch_KQL_application(self):
-        # Function to launch the KQL application
         subprocess.run(["python", "Tools/KQLConv.py"])
 
     def launch_DCAR_application(self):
-        # Function to launch the Document Creator AR application
         subprocess.run(["python", "Tools/DocumentCreatorAR.py"])
 
     def launch_DCPB_application(self):
-        # Function to launch the Document Creator PB application
         subprocess.run(["python", "Tools/DocumentCreatorPB.py"])
 
     def browse_folder(self):
-        # Function to allow the user to select a folder and set the selected folder_path
         folder_path = filedialog.askdirectory()
         if folder_path:
             self.folder_path_var.set(folder_path)
             self.refresh_json_count()
 
     def refresh_json_count(self):
-        # Function to refresh the JSON file count information
-        # Get the folder_path from the Entry widget
         folder_path = self.folder_path_var.get()
 
-        # Check if folder_path is not empty before proceeding
         if folder_path:
-            # Use the get_json_schemas function to count schemas
             schema_count = get_json_schemas(folder_path)
-
-            # Use the count_json_files function to count JSON files
             json_count, _ = count_json_files(folder_path)
 
-            # Clear existing items in the treeview
-            for item in self.tree.get_children():
-                self.tree.delete(item)
+            # Clear existing items in the treeviews
+            for item in self.tree_json_count.get_children():
+                self.tree_json_count.delete(item)
+
+            for item in self.tree_schema_count.get_children():
+                self.tree_schema_count.delete(item)
 
             # Insert new items in the treeview for JSON file count
             for category, count in json_count.items():
-                self.tree.insert("", "end", values=[category, count])
+                self.tree_json_count.insert("", "end", values=[category, count])
 
             # Insert new items in the treeview for schema count
             for schema, count in schema_count.items():
-                self.tree.insert("", "end", values=[schema, count])
+                self.tree_schema_count.insert("", "end", values=[schema, count])
 
 if __name__ == "__main__":
     root = tk.Tk()
---
+++ Tools/test.py
@@ -0,0 +1,63 @@
+import os
+import json
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import ttk  # Import ttk module for Treeview widget
+import subprocess
+
+def process_json_file(file_path, tree, parent, missing_title_count, missing_description_count):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        try:
+            data = json.load(file)
+            metadata = data.get('metadata', {})
+            title = metadata.get('title', '').strip()
+            description = metadata.get('description', '').strip()
+            
+            if not title:
+                missing_title_count[0] += 1
+                tree.insert(parent, 'end', text=f"{file_path} - Missing Title")
+                
+            if not description:
+                missing_description_count[0] += 1
+                tree.insert(parent, 'end', text=f"{file_path} - Missing Description")
+                
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON in file {file_path}: {e}")
+
+def process_folder(folder_path, tree):
+    missing_title_count = [0]
+    missing_description_count = [0]
+    
+    for root, dirs, files in os.walk(folder_path):
+        parent = tree.insert('', 'end', text=root, open=True)
+        for file in files:
+            if file.endswith('.json'):
+                file_path = os.path.join(root, file)
+                process_json_file(file_path, tree, parent, missing_title_count, missing_description_count)
+    
+    print(f"Total Playbooks Missing or Empty Title: {missing_title_count[0]}")
+    print(f"Total Playbooks Missing or Empty Description: {missing_description_count[0]}")
+
+def browse_playbook():
+   folder_path = filedialog.askdirectory(title="Select Folder with JSON Files")
+   if folder_path:
+       # Create the main window
+       root = tk.Tk()
+       root.title("Missing Titles and Descriptions")
+
+       # Create a Treeview widget
+       tree = ttk.Treeview(root)
+       tree.heading('#0', text='Missing Titles and Descriptions')
+       tree.pack(expand=True, fill='both')
+
+       # Process the folder and update the tree
+       process_folder(folder_path, tree)
+
+       # Run the Tkinter event loop
+       root.mainloop()
+
+# Run the Tkinter event loop
+root = tk.Tk()
+root.withdraw()  
+# Prompt user to select folder with JSON files
+browse_playbook()
