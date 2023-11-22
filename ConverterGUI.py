22810233e11e3c8742d9acffc13afaa269b73006
Tom Porter <Tom.Porter@satisnet.co.uk>
2023-11-22 13:19:36 +0000

    update GUI with live feed on amount of content

--- ConverterGUI.py
+++ ConverterGUI.py
@@ -1,18 +1,50 @@
+import os
 import tkinter as tk
 from tkinter import ttk, filedialog
 from PIL import Image, ImageTk
-import subprocess  
+import subprocess
+
+def count_json_files(folder_path):
+    json_count = {'Detections': 0, 'Hunting': 0, 'Playbooks': 0, 'Workbooks': 0, 'Total': 0}
+
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".json"):
+                json_count['Total'] += 1
+
+                # Categorize based on subdirectories
+                subdirectory = os.path.relpath(root, folder_path)
+                if subdirectory.startswith('Detections'):
+                    json_count['Detections'] += 1
+                elif subdirectory.startswith('Hunting'):
+                    json_count['Hunting'] += 1
+                elif subdirectory.startswith('Playbooks'):
+                    json_count['Playbooks'] += 1
+                elif subdirectory.startswith('Workbooks'):
+                    json_count['Workbooks'] += 1
+
+    return json_count
+
+def display_results_on_treeview(folder_path, tree):
+    result = count_json_files(folder_path)
+
+    # Clear existing items in the treeview
+    for item in tree.get_children():
+        tree.delete(item)
+
+    # Insert new items in the treeview
+    tree.insert("", "end", values=["Detections", result['Detections']])
+    tree.insert("", "end", values=["Hunting", result['Hunting']])
+    tree.insert("", "end", values=["Playbooks", result['Playbooks']])
+    tree.insert("", "end", values=["Workbooks", result['Workbooks']])
+    tree.insert("", "end", values=["Total", result['Total']])
 
 class MyGUI:
     def __init__(self, master):
         self.master = master
-        master.title("ARM-Converter-Tool")
-        master.geometry("380x400")
+        master.title("ARM Templating Tool")
+        master.geometry("680x380")
         master.resizable(False, False)
-
-        # Create a label with the title
-        title_label = ttk.Label(master, text="Welcome to My Tkinter GUI", font=("Helvetica", 16))
-        title_label.pack(pady=20)
 
         # Use a Frame to hold the background image
         self.background_frame = ttk.Frame(master)
@@ -23,14 +55,22 @@ class MyGUI:
         self.background_label = tk.Label(self.background_frame, image=self.background_image)
         self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
+        # Create a label with the title
+        title_label = ttk.Label(master, text="ARM Templating Tool", font=("Helvetica", 16))
+        title_label.place(relx=0.5, rely=0.05, anchor=tk.CENTER)
+
+        # Create a label with the author
+        author_label = ttk.Label(master, text="By Tom Porter", font=("Helvetica", 10))
+        author_label.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
+
         # Create a custom style for the buttons
         style = ttk.Style()
         style.theme_use("clam")
 
         # Configure general button style
         style.configure("TButton",
-                        font=("Helvetica", 12),
-                        padding=10,  
+                        font=("Helvetica", 10),
+                        padding=5,  
                         background="black",  
                         foreground="white"  
                         )
@@ -44,23 +84,45 @@ class MyGUI:
                         )
 
         # Create buttons using the custom style
+        button_spacing = 0.1
+        button_y = 0.25
+
         self.convert_playbook_button = ttk.Button(master, text="Convert Playbook", command=self.launch_playbook_application, style="TButton")
-        self.convert_playbook_button.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
+        self.convert_playbook_button.place(relx=0.1, rely=button_y, anchor=tk.W)
+        button_y += button_spacing
 
         self.convert_workbook_button = ttk.Button(master, text="Convert Workbook", command=self.launch_workbook_application, style="TButton")
-        self.convert_workbook_button.place(relx=0.5, rely=0.35, anchor=tk.CENTER)
+        self.convert_workbook_button.place(relx=0.1, rely=button_y, anchor=tk.W)
+        button_y += button_spacing
 
         self.convert_KQL_button = ttk.Button(master, text="Convert KQL", command=self.launch_KQL_application, style="TButton")
-        self.convert_KQL_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        self.convert_KQL_button.place(relx=0.1, rely=button_y, anchor=tk.W)
+        button_y += button_spacing
 
-        self.convert_KQL_button = ttk.Button(master, text="Document Creator AR", command=self.launch_DCAR_application, style="TButton")
-        self.convert_KQL_button.place(relx=0.75, rely=0.65, anchor=tk.CENTER)
+        self.convert_DCAR_button = ttk.Button(master, text="Document Creator AR", command=self.launch_DCAR_application, style="TButton")
+        self.convert_DCAR_button.place(relx=0.1, rely=button_y, anchor=tk.W)
+        button_y += button_spacing
 
-        self.convert_KQL_button = ttk.Button(master, text="Document Creator PB ", command=self.launch_DCPB_application, style="TButton")
-        self.convert_KQL_button.place(relx=0.25, rely=0.65, anchor=tk.CENTER)
-        #===
+        self.convert_DCPB_button = ttk.Button(master, text="Document Creator PB", command=self.launch_DCPB_application, style="TButton")
+        self.convert_DCPB_button.place(relx=0.1, rely=button_y, anchor=tk.W)
+        button_y += button_spacing
+
+        # Create an exit button
         self.exit_button = ttk.Button(master, text="Exit", command=master.destroy, style="Exit.TButton")
-        self.exit_button.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
+        self.exit_button.place(relx=0.1, rely=0.9, anchor=tk.W)
+
+        # Create a treeview to display JSON file count information
+        self.tree = ttk.Treeview(master, columns=("Category", "Count"), show="headings", height=5)
+        self.tree.heading("Category", text="Category")
+        self.tree.heading("Count", text="Count")
+        self.tree.place(relx=0.4, rely=0.37, anchor=tk.W)
+
+        # Create a button to refresh the JSON file count information
+        self.refresh_button = ttk.Button(master, text="Refresh", command=self.refresh_json_count, style="TButton")
+        self.refresh_button.place(relx=0.92, rely=0.6, anchor=tk.CENTER)
+
+        # Display the initial JSON file count information
+        self.refresh_json_count()
 
     # Launchers
     def launch_playbook_application(self):
@@ -77,6 +139,11 @@ class MyGUI:
     
     def launch_DCPB_application(self):
         subprocess.run(["python", "Tools/DocumentCreatorPB.py"])
+
+    def refresh_json_count(self):
+        # Hardcoded directory path
+        folder_path = "C:\Dev\ztf-sentinel"
+        display_results_on_treeview(folder_path, self.tree)
 
 if __name__ == "__main__":
     root = tk.Tk()
