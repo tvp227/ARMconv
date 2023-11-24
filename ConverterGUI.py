a67651b7a9eb4015caa1147d3108cb1b8ed087ce
Tom Porter <Tom.Porter@satisnet.co.uk>
2023-11-24 09:53:09 +0000

    Gui change

--- ConverterGUI.py
+++ ConverterGUI.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from tkinter import ttk, filedialog
 from PIL import Image, ImageTk
 import subprocess
+from datetime import datetime
 
 # Function to retrieve JSON schemas from a directory
 def get_json_schemas(directory):
@@ -26,6 +27,40 @@ def get_json_schemas(directory):
                     pass
 
     return schema_count
+
+# Function to retrieve JSON metadata from a directory
+def get_json_metadata(directory):
+    metadata_info = []
+
+    for root, dirs, files in os.walk(directory):
+        for file_name in files:
+            if file_name.endswith('.json'):
+                file_path = os.path.join(root, file_name)
+
+                try:
+                    # Get file creation and modification dates
+                    created_time = os.path.getctime(file_path)
+                    modified_time = os.path.getmtime(file_path)
+
+                    created_date = datetime.fromtimestamp(created_time).strftime('%Y-%m-%d %H:%M:%S')
+                    modified_date = datetime.fromtimestamp(modified_time).strftime('%Y-%m-%d %H:%M:%S')
+
+                    with open(file_path, 'r', encoding='utf-8') as file:
+                        data = json.load(file)
+
+                        # Extract metadata information
+                        metadata = {
+                            'File Name': file_name,
+                            'Creation Date': created_date,
+                            'Modified Date': modified_date,
+                            # Add more metadata fields as needed
+                        }
+
+                        metadata_info.append(metadata)
+                except (json.JSONDecodeError, FileNotFoundError, UnicodeDecodeError):
+                    pass
+
+    return metadata_info
 
 # Function to count JSON files in the specified folder_path
 def count_json_files(directory):
@@ -63,8 +98,9 @@ def count_json_files(directory):
     return json_count, schema_count
 
 # Function to display JSON file and schema count information in the treeview
-def display_results_on_treeview(directory, tree_json_count, tree_schema_count):
+def display_results_on_treeview(directory, tree_json_count, tree_schema_count, tree_metadata_count):
     json_count, schema_count = count_json_files(directory)
+    metadata_info = get_json_metadata(directory)
 
     # Clear existing items in the first treeview (JSON file count)
     for item in tree_json_count.get_children():
@@ -74,6 +110,10 @@ def display_results_on_treeview(directory, tree_json_count, tree_schema_count):
     for item in tree_schema_count.get_children():
         tree_schema_count.delete(item)
 
+    # Clear existing items in the third treeview (metadata count)
+    for item in tree_metadata_count.get_children():
+        tree_metadata_count.delete(item)
+
     # Insert new items in the first treeview for JSON file count
     for category, count in json_count.items():
         tree_json_count.insert("", "end", values=[category, count])
@@ -81,6 +121,10 @@ def display_results_on_treeview(directory, tree_json_count, tree_schema_count):
     # Insert new items in the second treeview for schema count
     for schema, count in schema_count.items():
         tree_schema_count.insert("", "end", values=[schema, count])
+
+    # Insert new items in the third treeview for metadata count
+    for metadata in metadata_info:
+        tree_metadata_count.insert("", "end", values=[metadata['File Name'], metadata['Creation Date'], metadata['Modified Date']])
 
 class MyGUI:
     def __init__(self, master):
@@ -129,48 +173,48 @@ class MyGUI:
 
         button_spacing = 0.1
         button_y = 0.25
-        
-        #Convert Playbook
+
+        # Convert Playbook
         self.convert_playbook_button = ttk.Button(master, text="Convert Playbook", command=self.launch_playbook_application, style="TButton")
         self.convert_playbook_button.place(relx=0.1, rely=button_y, anchor=tk.W)
         button_y += button_spacing
-        #Convert Workbook
+        # Convert Workbook
         self.convert_workbook_button = ttk.Button(master, text="Convert Workbook", command=self.launch_workbook_application, style="TButton")
         self.convert_workbook_button.place(relx=0.1, rely=button_y, anchor=tk.W)
         button_y += button_spacing
-        #Convert KQL button
+        # Convert KQL button
         self.convert_KQL_button = ttk.Button(master, text="Convert KQL", command=self.launch_KQL_application, style="TButton")
         self.convert_KQL_button.place(relx=0.1, rely=button_y, anchor=tk.W)
         button_y += button_spacing
-        #Document Creator AR button
+        # Document Creator AR button
         self.convert_DCAR_button = ttk.Button(master, text="Document Creator AR", command=self.launch_DCAR_application, style="TButton")
         self.convert_DCAR_button.place(relx=0.1, rely=button_y, anchor=tk.W)
         button_y += button_spacing
-        #Document Creator PB button
+        # Document Creator PB button
         self.convert_DCPB_button = ttk.Button(master, text="Document Creator PB", command=self.launch_DCPB_application, style="TButton")
         self.convert_DCPB_button.place(relx=0.1, rely=button_y, anchor=tk.W)
         button_y += button_spacing
-        #PS Button 
+        # PS Button
         self.convert_PS1_button = ttk.Button(master, text="AzureCLI", command=self.launch_PS1_application, style="PS.TButton")
         self.convert_PS1_button.place(relx=0.1, rely=button_y, anchor=tk.W)
         button_y += button_spacing
-        #Dir Display Box
+        # Dir Display Box
         self.folder_path_var = tk.StringVar()
         self.folder_path_entry = ttk.Entry(master, textvariable=self.folder_path_var, state='readonly', font=("Helvetica", 10))
         self.folder_path_entry.place(relx=0.86, rely=0.18, anchor=tk.CENTER)
-        #Browse Button
+        # Browse Button
         self.browse_button = ttk.Button(master, text="Browse", command=self.browse_folder, style="TButton")
         self.browse_button.place(relx=0.78, rely=0.83, anchor=tk.CENTER)
-        #Refresh Button
+        # Refresh Button
         self.refresh_button = ttk.Button(master, text="Refresh", command=self.refresh_json_count, style="TButton")
         self.refresh_button.place(relx=0.9, rely=0.83, anchor=tk.CENTER)
         self.refresh_json_count()
-        #Exit Button
+        # Exit Button
         self.exit_button = ttk.Button(master, text="Exit", command=master.destroy, style="Exit.TButton")
         self.exit_button.place(relx=0.1, rely=button_y, anchor=tk.W)
         button_y += button_spacing
 
-        #Tree styling
+        # Tree styling
         style.configure("Treeview",
                         background="white",
                         fieldbackground="grey"
@@ -186,28 +230,30 @@ class MyGUI:
                   background=[("selected", "grey4")],
                   foreground=[("selected", "white")]
                   )
-        #Json Count Tree
+        # Json Count Tree
         self.tree_json_count = ttk.Treeview(master, columns=("Category", "Count"), show="headings", height=5)
 
         self.tree_json_count.heading("Category", text="Category", anchor=tk.CENTER)
         self.tree_json_count.heading("Count", text="Count", anchor=tk.CENTER)
 
         self.tree_json_count.place(relx=0.295, rely=0.52, anchor=tk.W, width=530, height=120)
-        #Schema Count Tree
+        # Schema Count Tree
         self.tree_schema_count = ttk.Treeview(master, columns=("Schema", "Count"), show="headings", height=5)
 
         self.tree_schema_count.heading("Schema", text="Schema", anchor=tk.CENTER)
         self.tree_schema_count.heading("Count", text="Count", anchor=tk.CENTER)
 
         self.tree_schema_count.place(relx=0.295, rely=0.71, anchor=tk.W, width=530, height=120)
-        #Meta Data Tree
-        self.tree_MetaData_count = ttk.Treeview(master, columns=("Schema", "Count"), show="headings", height=5)
+        # Meta Data Tree
+        self.tree_MetaData_count = ttk.Treeview(master, columns=("File Name", "Creation Date", "Modified Date"), show="headings", height=5)
 
-        self.tree_MetaData_count.heading("Schema", text="Schema", anchor=tk.CENTER)
-        self.tree_MetaData_count.heading("Count", text="Count", anchor=tk.CENTER)
+        self.tree_MetaData_count.heading("File Name", text="File Name", anchor=tk.CENTER)
+        self.tree_MetaData_count.heading("Creation Date", text="Creation Date", anchor=tk.CENTER)
+        self.tree_MetaData_count.heading("Modified Date", text="Modified Date", anchor=tk.CENTER)
 
         self.tree_MetaData_count.place(relx=0.295, rely=0.32, anchor=tk.W, width=530, height=120)
-    #Launchers
+
+    # Launchers
     def launch_playbook_application(self):
         subprocess.run(["python", "Tools/PlayBookConv.py"])
 
@@ -222,7 +268,7 @@ class MyGUI:
 
     def launch_DCPB_application(self):
         subprocess.run(["python", "Tools/DocumentCreatorPB.py"])
-    
+
     def launch_PS1_application(self):
         subprocess.run(["powershell", "Powershell/Playbook_ARM_Template_Generator.ps1"])
 
@@ -238,6 +284,7 @@ class MyGUI:
         if folder_path:
             schema_count = get_json_schemas(folder_path)
             json_count, _ = count_json_files(folder_path)
+            metadata_info = get_json_metadata(folder_path)
 
             # Clear existing items in the treeviews
             for item in self.tree_json_count.get_children():
@@ -246,6 +293,9 @@ class MyGUI:
             for item in self.tree_schema_count.get_children():
                 self.tree_schema_count.delete(item)
 
+            for item in self.tree_MetaData_count.get_children():
+                self.tree_MetaData_count.delete(item)
+
             # Insert new items in the treeview for JSON file count
             for category, count in json_count.items():
                 self.tree_json_count.insert("", "end", values=[category, count])
@@ -253,6 +303,11 @@ class MyGUI:
             # Insert new items in the treeview for schema count
             for schema, count in schema_count.items():
                 self.tree_schema_count.insert("", "end", values=[schema, count])
+
+            # Insert new items in the treeview for metadata count
+            for metadata in metadata_info:
+                self.tree_MetaData_count.insert("", "end", values=[metadata['File Name'], metadata['Creation Date'], metadata['Modified Date']])
+
 
 if __name__ == "__main__":
     root = tk.Tk()
